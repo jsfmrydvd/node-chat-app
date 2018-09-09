67c708c8aeb4c57c88de1c3e184831a3d40bbb2c
Josef David <josefmarydavid@gmail.com>
2018-09-10 01:46:34 +0800

    added admin broadcast for new user

--- server/server.js
+++ server/server.js
@@ -38,6 +38,18 @@ io.on('connection', (socket) => {
     //     console.log('createEmail', newEmail);
     // })
 
+    //socket.emit from Admin text "welcome to the app"
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+    //socket.broadcast.emit from admin text new user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
         io.emit('newMessage', {
@@ -45,6 +57,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
