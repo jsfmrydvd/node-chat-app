9d5f826c11263d91b87cf0186052672bea54b3cd
Josef David <josefmarydavid@gmail.com>
2018-09-12 14:39:41 +0800

    Added autoscroll

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 app.use(express.static(publicPath));
-
 io.on('connection', (socket) => {
     console.log('New user connected');
 
